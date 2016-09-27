db3ed7ee6dc5484ba05ade0ea3e33f858af46fe6
riccardoscalet <rscalet@techimp.com>
2016-09-27 17:15:34 +0200

    asd

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = function(db){
     return {
         addAsset: function(db) {
             return "aaa";
+            
+                         
         },
 
         modifyState: function() {
